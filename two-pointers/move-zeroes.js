5db60cf18c3df5a06b98b64ca0332c7342f0878a
dlaller <dlaller1988@gmail.com>
2025-11-11 17:44:19 +0530

    move Zeroes in and array

---
+++ two-pointers/move-zeroes.js
@@ -0,0 +1,32 @@
+// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+// Note that you must do this in-place without making a copy of the array.
+
+ 
+
+// Example 1:
+
+// Input: nums = [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+// Example 2:
+
+// Input: nums = [0]
+// Output: [0]
+
+var moveZeroes = function(nums) {
+    let i =0;
+    let lastNonXeor=0
+    while(i<nums.length){
+        if(nums[i]!==0){
+                nums[lastNonXeor]=nums[i];
+                lastNonXeor++;
+        }
+        i++;
+    }
+    
+    for (let i = lastNonXeor; i < nums.length; i++) {
+        nums[i] = 0;
+      }
+
+    return nums;
+};
